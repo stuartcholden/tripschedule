69fac2c2b88034a46b97e94ed094e07aedf8e106
Stu <stuart.c.holden@gmail.com>
2019-07-12 11:51:50 -0400

    Deleted some files with private information

--- tripemail.py
+++ tripemail.py
@@ -1,7 +1,7 @@
 import csv, smtplib, ssl, xlrd, datetime
 
 from_address = "kandalore.trippers@gmail.com"
-password = "standardissuechotch"
+password = "shalominthehome"
 to_address = "tripdirector@kandalore.com"
 
 
@@ -24,4 +24,10 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
             if row['Start Date'] == str(inthreedays):
                 message = "Subject: Trip " + row['TripID'] + ", " + dayleavingtext + "\n\n" + "Hi " + row['Tripper 1'] + ",\n\nYou're leading the " + row['Route'] + " " + dayleavingtext + " with " + row['Staff1DisplayName'] + " Here's what you'll need:\n\nDish kits: " + row['Dish Kits'] + "\nAquatabs: " + row['Aquatabs'] + "\nBoats: " + row['Boats'] + "\nTents: " + row['Tents'] + "\nWhistles: " + row['Whistles'] + "\nPaper bags: " + row['Paper Bags'] + "\nRolls of toilet paper (pack the extra one seperately to avoid disaster): " + row['Toilet Paper'] + "\nBarrels: " + row['Barrels'] + "\n\n Your campers are:\n\n" + row['Camper 1'] + "\n" + row['Camper 2'] + "\n" + row['Camper 3'] + "\n" + row['Camper 4'] + "\n" + row['Camper 5'] + "\n" + row['Camper 6'] + "\n" + row['Camper 7'] + "\n" + row['Camper 8'] + "\n" + row['Camper 9'] + "\n" + row['Camper 10'] + "\n" + row['Camper 11'] + "\n" + row['Camper 12'] + "\n" + row['Camper 13'] + "\n" + row['Camper 14'] + "\n" + row['Camper 15'] + "\n" + row['Camper 16'] + "\n" + row['Camper 17'] + "\n" + row['Camper 18'] + "\n" + row['Camper 19'] + "\n" + row['Camper 20'] + "\n\nSincerely,\n\nStu" + "\n\n" 
 
-                server.sendmail("tripdirector@kandalore.com",row['email'],message)
+                server.sendmail(from_address,testemail,message)
+
+with open("email.csv",encoding="utf-8") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        if row['Start Date'] == str(inthreedays):
+            testemail = "row['stuemail']"
